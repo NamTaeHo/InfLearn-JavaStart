addf26afb498b0ce54edc639c0e421182335c0d0
NangFeh <han127kr@gmail.com>
2023-12-05 15:22:21 +0900

    Init Commit

---
+++ src/variable/Var8.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class Var8 {
+
+    byte b = 127; // -128 ~ 127
+    short s = 32767;// -32,768 ~ 32,767
+    int i = 2147483647; // (약 -20억 ~ 20억)
+    long l = 922337036854775807L; // 엄청큼 -92경 ~ 92경
+
+
+    float f = 10.0f;
+    double d = 10.0;
+
+
+}
